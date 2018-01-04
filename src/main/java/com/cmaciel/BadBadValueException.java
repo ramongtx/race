bd05ae3bda9e57e91fb9dc6388acef021e0a502b
Ramon Carvalho Maciel <ramongtx@gmail.com>
2018-01-04 12:36:58 -0300

    Add TimeDuration class

---
+++ src/main/java/com/cmaciel/BadBadValueException.java
@@ -0,0 +1,10 @@
+package com.cmaciel;
+
+public class BadBadValueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/cmaciel/TimeDuration.java
@@ -0,0 +1,33 @@
+package com.cmaciel;
+
+public class TimeDuration {
+	private int seconds;
+
+	public TimeDuration(int seconds) throws BadBadValueException {
+		this.setSeconds(seconds);
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(int seconds) throws BadBadValueException {
+		if (seconds < 0) throw new BadBadValueException();
+		this.seconds = seconds;
+	}
+	
+	public String toString() {
+		int hours = seconds / 3600;
+		int minutes = ((seconds % 3600) / 60);
+		int remainder = seconds % 60;
+		
+		if (hours > 0) {
+			return String.format("%2dh %2m %2ds", hours, minutes, remainder);
+		} else if (minutes > 0) {
+			return String.format("%2m %2ds", minutes, remainder);
+		}
+		
+		return String.format("%2ds", remainder);
+	}
+
+}
