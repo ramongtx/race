d43792a4ae99ce9568c9d532ba16963e2c0caf9d
Ramon Carvalho Maciel <ramongtx@gmail.com>
2018-01-04 13:50:31 -0300

    Fix identation and add comment

--- src/main/java/com/cmaciel/RaceResults.java
+++ src/main/java/com/cmaciel/RaceResults.java
@@ -2,18 +2,17 @@ package com.cmaciel;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class RaceResults {
 	private class RaceResult implements Comparable<RaceResult> {
-	    private String tagNumber;
-	    private TimeDuration time;
-	    
-	    public RaceResult(String tagNumber, TimeDuration time) {
+		private String tagNumber;
+		private TimeDuration time;
+
+		public RaceResult(String tagNumber, TimeDuration time) {
 			this.setTagNumber(tagNumber);
 			this.setTime(time);
-	    }
-	    
+		}
+
 		public void setTagNumber(String tagNumber) {
 			this.tagNumber = tagNumber;
 		}
@@ -23,7 +22,7 @@ public class RaceResults {
 		public void setTime(TimeDuration time) {
 			this.time = time;
 		}
-		
+
 		public String toString() {
 			return String.format("%s - %s", tagNumber, time.toString());
 		}
@@ -32,33 +31,38 @@ public class RaceResults {
 			return this.time.compareTo(other.getTime());
 		}
 	}
-	
+
 	private ArrayList<RaceResult> resultsList;
-	
+
 	public RaceResults() {
 		resultsList = new ArrayList<RaceResult>(); 
 	}
-	
+
 	public void onNewResult(String tagNumber, TimeDuration resultTime) {
 		resultsList.add(new RaceResult(tagNumber, resultTime));
 	}
-	
+
 	public void printResults() {
 		Collections.sort(resultsList);
 		for (RaceResult result: resultsList) {
 			System.out.println(result.toString());
-	   }
+		}
 	}
-	
+
 	public static void main(String[] args) throws BadBadValueException {
+
+		// One thing that can go wrong with the system is that
+		// depending on the examples, it can throw an exception
+
 		RaceResults raceResults = new RaceResults();
+
 		raceResults.onNewResult("AAA", new TimeDuration(300));
 		raceResults.onNewResult("BBB", new TimeDuration(340));
 		raceResults.onNewResult("CCC", new TimeDuration(124));
 		raceResults.onNewResult("DDD", new TimeDuration(80));
 		raceResults.onNewResult("EEE", new TimeDuration(730));
 		raceResults.onNewResult("FFF", new TimeDuration(7134));
-		
+
 		raceResults.printResults();
 	}
 
--- src/test/java/race/TimeDurationTest.java
+++ src/test/java/race/TimeDurationTest.java
@@ -11,20 +11,35 @@ public class TimeDurationTest {
 
 	@Test
 	public void stringifies() throws BadBadValueException {
-		System.out.println(new TimeDuration(732).toString());
-		System.out.println(new TimeDuration(7242).toString());
-		System.out.println(new TimeDuration(0).toString());
 		assertEquals(new TimeDuration(732).toString(), "12m 12s");
 		assertEquals(new TimeDuration(7242).toString(), "2h 0m 42s");
 		assertEquals(new TimeDuration(0).toString(), "0s");
 	}
-	
+
 	@Test
-	public void testException() {
-	    try {
-	        new TimeDuration(-1);
-	        fail("Expected a BadBadValueException to be thrown");
-	    } catch (BadBadValueException aBadBadValueException) {
-	    }
+	public void testInitializeException() {
+		try {
+			new TimeDuration(-1);
+			fail("Expected a BadBadValueException to be thrown");
+		} catch (BadBadValueException aBadBadValueException) {
+		}
+	}
+
+	@Test
+	public void testSetSecondsException() {
+		try {
+			TimeDuration timeDuration = new TimeDuration(0);
+			assertEquals(timeDuration.getSeconds(), 0);
+			timeDuration.setSeconds(-1);
+			fail("Expected a BadBadValueException to be thrown");
+		} catch (BadBadValueException aBadBadValueException) {
+		}
+	}
+
+	@Test
+	public void comparation() throws BadBadValueException {
+		assertTrue(new TimeDuration(10).compareTo(new TimeDuration(20)) < 0);
+		assertTrue(new TimeDuration(32).compareTo(new TimeDuration(31)) > 0);
+		assertTrue(new TimeDuration(58).compareTo(new TimeDuration(58)) == 0);
 	}
 }
